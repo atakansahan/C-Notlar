90282b47cae8ebcfdfd35bc4ad4d095f8c863a82
atakan.sahan <atakan.sahan@ALS-YG39104-J09>
2016-11-30 12:03:57 +0200

    update

---
+++ for/for/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ford
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Giriş Tanım
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Atakan");
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region WHİLE FOR
+            //int n = 10, fakto = 1, counter = 1;
+
+            //while (counter <=n)
+            //{
+            //    fakto *= counter;
+            //    counter++;
+            //}
+            //Console.WriteLine(fakto);
+            //Console.ReadKey();
+            //int n = 5, fakto = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    fakto *= i;
+            //}
+            //Console.WriteLine(fakto);
+            //Console.ReadKey();
+            #endregion
+
+            #region BirdenFazlaKontrol
+            //bool durum = true;
+            //for (int i=0; durum ==true && i< 100;i++)
+            //{
+            //    if (i * 2 >20)
+            //    {
+            //        durum = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0} iken devam ediyorum", i);
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region BreakContinue
+            //for (int i = 0; i < 100; i= i+ 5)
+            //{
+            //    if (i*2 < 20)
+            //    {
+            //        //break;
+            //        continue;
+            //    }
+            //    Console.WriteLine("{0} iken devam ediyorum", i);
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region [1,n] aralığındaki sayıların toplamı
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    toplam += i;
+            //    Console.WriteLine("i :{0\t Toplam : {1}", i, toplam);
+            //}
+            //Console.WriteLine("Genel Toplam:" + toplam);
+            //Console.ReadKey();
+            #endregion
+            
+        }
+    }
+}
