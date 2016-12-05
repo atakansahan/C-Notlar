3d2b72e146dda05c5e12c3a1ccb64f5dc415032a
atakan.sahan <atakan.sahan@ALS-YG39104-J09>
2016-12-05 10:40:50 +0200

    Update

---
+++ KareSekil/KareSekil/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KareSekil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Kare
+            /*
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == 0 || i == 4 || j == 0 || j == 4)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            /*/
+            #endregion
+
+            #region Dikdörtgen
+            /*
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i == 0 || i == 4 || j == 0 || j == 7)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            /*/
+            #endregion
+
+            #region Paralelkenar
+/*
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (i == 0 && j >=2 )
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 1 && (j == 1 || j == 4))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    if (i == 2 && j <= 3)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            /*/
+            #endregion
+        }
+    }
+}
