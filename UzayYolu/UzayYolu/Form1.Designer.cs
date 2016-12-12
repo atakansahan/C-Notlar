45101f8dc297b55b38d66cad5aa56011af058c1e
atakan.sahan <atakan.sahan@ALS-YG39506-J09>
2016-12-12 11:51:33 +0200

    Update

---
+++ UzayYolu/UzayYolu/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace UzayYolu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SpaceShip = new System.Windows.Forms.PictureBox();
+            this.Rocket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.SpaceShip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rocket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SpaceShip
+            // 
+            this.SpaceShip.BackColor = System.Drawing.Color.Transparent;
+            this.SpaceShip.Image = ((System.Drawing.Image)(resources.GetObject("SpaceShip.Image")));
+            this.SpaceShip.Location = new System.Drawing.Point(12, 118);
+            this.SpaceShip.Name = "SpaceShip";
+            this.SpaceShip.Size = new System.Drawing.Size(125, 115);
+            this.SpaceShip.TabIndex = 0;
+            this.SpaceShip.TabStop = false;
+            // 
+            // Rocket
+            // 
+            this.Rocket.BackColor = System.Drawing.Color.Transparent;
+            this.Rocket.Image = ((System.Drawing.Image)(resources.GetObject("Rocket.Image")));
+            this.Rocket.Location = new System.Drawing.Point(143, 173);
+            this.Rocket.Name = "Rocket";
+            this.Rocket.Size = new System.Drawing.Size(20, 19);
+            this.Rocket.TabIndex = 1;
+            this.Rocket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(809, 451);
+            this.Controls.Add(this.Rocket);
+            this.Controls.Add(this.SpaceShip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.SpaceShip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rocket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SpaceShip;
+        private System.Windows.Forms.PictureBox Rocket;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ UzayYolu/UzayYolu/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UzayYolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Rocket.Visible = false;
+            
+        }
+        int genislik, yukseklik;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    SpaceShip.Left -= 10;
+                    break;
+                case Keys.Right:
+                    SpaceShip.Left += 10;
+                    break;
+                case Keys.Up:
+                    SpaceShip.Top -= 10;
+                    break;
+                case Keys.Down:
+                    SpaceShip.Top += 10;
+                    break;
+
+                case Keys.Space:
+                    int x = SpaceShip.Location.X + SpaceShip.Width / 2;
+                    int y = SpaceShip.Location.Y + SpaceShip.Height / 2;
+                    Rocket.Location = new Point(x, y);
+                    Rocket.Visible = true;
+                    timer1.Start();
+                    break;
+
+                default:
+                    break;
+
+                
+            }
+                
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            genislik = Form1.ActiveForm.Width;
+            yukseklik = Form1.ActiveForm.Height;
+            Rocket.Left += 10;
+        }
+    }
+}
