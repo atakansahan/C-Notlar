78e13a159f08e5f3c6c5c57672d13ac0ae142c78
atakan.sahan <atakan.sahan@ALS-YG39104-J09>
2017-01-10 11:58:39 +0200

    SQL

---
+++ Database_Sample/Database_Sample/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,30 @@
+﻿
+SELECT CustomerID, ContactName FROM Customers;
+GO
+
+SELECT * FROM Customers;
+GO
+
+SELECT COUNT(ContactName)FROM Customers WHERE Country = 'Germany';
+GO
+
+SELECT * FROM Customers WHERE Country = 'Brazil';
+GO
+
+SELECT * FROM Customers WHERE Country = 'Brazil' AND City = 'Sao Paulo' ;
+GO
+
+SELECT CustomerID, ContactName FROM Customers;
+GO
+
+SELECT * FROM Customers;
+GO
+
+SELECT COUNT(ContactName)FROM Customers WHERE Country = 'Germany';
+GO
+
+SELECT * FROM Customers WHERE Country = 'Brazil';
+GO
+
+SELECT * FROM Customers WHERE Country = 'Brazil' AND City = 'Sao Paulo' ;
+GO
